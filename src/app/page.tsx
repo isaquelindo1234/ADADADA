35011cd628a59feab0efca2b5da78b6a2f8cc832
Isaque Barbosa Oliveira <IsAuQuE1234@gmail.com>
2025-10-16 04:43:03 +0000

    remova essa seção  (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { AuthorityBar } from '@/components/landing/AuthorityBar';
 import { SpecialistSection } from '@/components/landing/SpecialistSection';
 import { FaqSection } from '@/components/landing/FaqSection';
-import { GuaranteeSection } from '@/components/landing/GuaranteeSection';
 import { HeroSection } from '@/components/landing/HeroSection';
 import { HowItWorksSection } from '@/components/landing/HowItWorksSection';
 import { OfferSection } from '@/components/landing/OfferSection';
@@ -19,7 +18,6 @@ export default function Home() {
         <HowItWorksSection />
         <SpecialistSection />
         <TestimonialSection />
-        <GuaranteeSection />
         <FaqSection />
       </main>
     </div>
