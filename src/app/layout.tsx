a38e42f021a9a53c4909e565297c29cd4d321f16
Isaque Barbosa Oliveira <IsAuQuE1234@gmail.com>
2025-10-16 05:48:38 +0000

    adicione esse script de utm
    <script
      src="https://cdn.utmify.com.br/s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
             document.head.appendChild(a);
           `}
         </Script>
+        <Script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          async
+          defer
+        ></Script>
         {children}
         <Toaster />
       </body>
