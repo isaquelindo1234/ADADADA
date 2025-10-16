e104f8e099a53d8ebed213b2def43d50fde38beb
Isaque Barbosa Oliveira <IsAuQuE1234@gmail.com>
2025-10-16 04:16:57 +0000

    coloque em destaque 'interruptor de arrepentimiento' (_for element h1_)

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -40,7 +40,7 @@ export function HeroSection() {
       <div className="container mx-auto px-4 flex flex-col items-center">
         <div className="max-w-2xl">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-headline font-bold text-white leading-tight mb-6">
-            Existe un 'interruptor de arrepentimiento' en el cerebro masculino. <br /> Pocas mujeres saben cómo activarlo.
+            Existe un <span className="text-primary">'interruptor de arrepentimiento'</span> en el cerebro masculino. <br /> Pocas mujeres saben cómo activarlo.
           </h1>
           <div
             className="wistia_responsive_padding"
