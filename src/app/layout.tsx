2cefd13ffb4a30a7b9ea33de6f4863507bd12290
Isaque Barbosa Oliveira <IsAuQuE1234@gmail.com>
2025-10-18 17:23:12 +0000

    O DESGRAÇA DO WISTIA NÃO SO DO PIXEL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Inter:wght@400;600&display=swap" rel="stylesheet" />
+        <Script src="https://fast.wistia.com/assets/external/E-v1.js" async />
       </head>
       <body className="font-body antialiased bg-background">
         {children}
