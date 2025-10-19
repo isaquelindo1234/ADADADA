c68c3f8b63b101f4b46d1784f092778b99b89e36
Isaque Barbosa Oliveira <IsAuQuE1234@gmail.com>
2025-10-19 18:07:21 +0000

    remova o pixel do facebook deixa somente da UTmify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,25 +40,6 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="font-body antialiased bg-background">
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '804787918934564');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        <noscript>
-          <img height="1" width="1" style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=804787918934564&ev=PageView&noscript=1"
-          />
-        </noscript>
         {children}
         <Toaster />
       </body>
