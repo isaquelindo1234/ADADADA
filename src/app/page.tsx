187957742362fa7350053ea37a9feea9e91d2b22
Isaque Barbosa Oliveira <IsAuQuE1234@gmail.com>
2026-03-25 19:06:42 +0000

    REMOVA ESSA PARTE  (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,11 @@ import { InfoSection } from '@/components/landing/InfoSection';
 import { TestimonialSection } from '@/components/landing/TestimonialSection';
 import { FaqSection } from '@/components/landing/FaqSection';
 import { Footer } from '@/components/landing/Footer';
-import { UrgencyBanner } from '@/components/landing/UrgencyBanner';
 import { MidPageOfferSection } from '@/components/landing/MidPageOfferSection';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <UrgencyBanner />
       <main className="flex-1">
         <HeroSection />
         <InfoSection />
